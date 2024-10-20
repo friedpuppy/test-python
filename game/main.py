21af337a88d4dad3e38ec5141f890b7377e22b8d
Callum Donnelly <callum.donnelly74@gmail.com>
2024-10-20 14:40:46 +0100

    creation of class

--- game/main.py
+++ game/main.py
@@ -5,4 +5,12 @@ import sys
 
 class Game:
     def __init__(self):
-        pygame.init
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font('Tahoma', 32)
+        self.running = True
+
+    def new(self):
+        # new game start
+        self.playing = True
